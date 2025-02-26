b1c8ca041ea614ae5c20df927e80cbabe2586d2d
LB-Squishy <laurentleboulbar@gmail.com>
2025-02-26 15:43:00 +0100

    Ajout des migrations pour les entités Author, Book, Comment et Editor, ainsi que les relations entre elles.

--- migrations/Version20250226143853.php
+++ migrations/Version20250226143853.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250226135708 extends AbstractMigration
+final class Version20250226143853 extends AbstractMigration
 {
     public function getDescription(): string
     {
